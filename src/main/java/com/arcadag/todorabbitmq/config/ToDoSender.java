cdb71a4551ca42fae1c88ceb8b6282d7524b06d1
arcadag <piknik0711@yandex.ru>
2023-02-22 22:14:23 +0300

    modified ToDoSender

--- src/main/java/com/arcadag/todorabbitmq/config/ToDoSender.java
+++ src/main/java/com/arcadag/todorabbitmq/config/ToDoSender.java
@@ -2,17 +2,31 @@ package com.arcadag.todorabbitmq.config;
 
 import com.arcadag.todorabbitmq.domain.ToDo;
 import com.arcadag.todorabbitmq.rmq.ToDoProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@EnableScheduling
 @Configuration
 public class ToDoSender {
-   @Bean
-    public CommandLineRunner sendToDos(@Value("${todo.amqp.queue}") String destination, ToDoProducer producer) {
-        return args -> {
-            producer.sendTo(destination, new ToDo("Do homework. Example for behavior pattern Listener"));
-        };
+
+    @Autowired
+    private ToDoProducer producer;
+    @Value("${todo.amqp.queue}")
+    private String destination;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+   @Scheduled(fixedRate = 500L)
+   public void sendToDos() {
+            producer.sendTo(
+                    destination,
+                    new ToDo("Do homework. Example for behavior pattern Listener " + dateFormat.format(new Date())));
+
     }
 }
